548431a50d1fb9cfaf51a99c1378403efb212e02
alehow84 <131718604+alehow84@users.noreply.github.com>
2024-01-10 17:24:23 +0000

    add progress on "counter intelligence" kata: 3 tests passed, fourth failing

--- week_2_katas/__tests__/counter-intelligence.test.js
+++ week_2_katas/__tests__/counter-intelligence.test.js
@@ -1,7 +1,33 @@
 const { counterIntelligence } = require('../counter-intelligence');
 
 describe('counterIntelligence()', () => {
-  it('', () => {
-    // first test here
+  it('returns a string', () => {
+    const input = 'Y'
+    const actualOutput = counterIntelligence(input)
+
+    expect(typeof actualOutput).toBe('string');
   });
+  it('returns the correct string when the encrypted msg has been shifted by 1 character and is only 1 character long', () => {
+    const input = 'Y'
+    const actualOutput = counterIntelligence(input)
+    const expectedOutput = 'X'
+
+    expect(actualOutput).toBe(expectedOutput);
+  });
+  it('returns the correct string when the encrypted msg has been shifted by 1 character and has multiple characters', () => {
+    const input = 'BCD Y'
+    const actualOutput = counterIntelligence(input)
+    const expectedOutput = 'ABC X'
+
+    expect(actualOutput).toBe(expectedOutput)
+
+  })
+  it('returns the correct string when the encrypted msg has been shifted by any character and has multiple characters', () => {
+    const input = 'NKRRU YCKKZNKGXZ D'
+    const actualOutput = counterIntelligence(input)
+    const expectedOutput = 'HELLO SWEETHEART X'
+
+    expect(actualOutput).toBe(expectedOutput)
+
+  })
 });
--- week_2_katas/counter-intelligence.js
+++ week_2_katas/counter-intelligence.js
@@ -1,6 +1,35 @@
-// Please do not change the name of this function
 const counterIntelligence = (str) => {
-  // Your code here
+  const lastCharsCode = str.charCodeAt(str.length-1)
+  // console.log(lastCharsCode, 'lastCharsCode on line 3')
+  let charShift = 88 - lastCharsCode
+  // console.log(charShift,'charShift on line 4')
+  const strArr = str.split('')
+  const decodedStrArr = strArr.map((letter)=>{
+    if (letter === ' ') {
+      return letter
+    } else {
+    const letterIndex = strArr.indexOf(letter)
+    let letterCode = strArr.join('').charCodeAt(letterIndex)
+    if(charShift < 0) {
+    letterCode+= charShift 
+    letter = String.fromCharCode(letterCode)
+    return letter
+    } else {
+      // console.log(letter, letterCode, charShift, String.fromCharCode(letterCode - charShift), 'letterCode on line 18')
+      /*
+      something is wrong here, so far for the last test, the X returns as expected, but the difference between the expected char codes and the received char codes is 26. Confused!
+      */
+      letterCode = letterCode + charShift
+      console.log(letterCode, 'letterCode on line 20')
+      letter = String.fromCharCode(letterCode)
+      return letter
+    }
+    }
+    
+  })
+  return decodedStrArr.join('')
+  
+  
 };
 
 module.exports = { counterIntelligence };
