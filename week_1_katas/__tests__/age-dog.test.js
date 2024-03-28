ebc104893a9141230b5ee4f91beebacbba5a4350
Alex Howlett <alex.howlett@live.co.uk>
2024-03-28 16:40:43 +0000

    install typescript and complete age-dog kata in javascript

--- week_1_katas/__tests__/age-dog.test.js
+++ week_1_katas/__tests__/age-dog.test.js
@@ -1,7 +1,24 @@
 const ageDog = require('../age-dog.js');
 
+/*
+This function will take a single dog object with an age property and a number. You should increment the age property by the provided number and return the aged dog. The original dog object must not be mutated!
+
+- solve problem in js
+-refactor using typescript
+*/
+
 describe('ageDog', () => {
-  it('', () => {
-    // first test here
+  it('Should return an object that does not mutate the passed object', () => {
+    const startDog = { name: 'Milo', age: 2 };
+
+    ageDog(startDog, 1);
+
+    expect(startDog.age).toBe(2);
+    expect(typeof ageDog(startDog, 1)).toBe('object');
+  });
+  it('should return an object with the age property incremented by the given number argument', () => {
+    const startDog = { name: 'Milo', age: 2 };
+
+    expect(ageDog(startDog, 3)).toEqual({ name: 'Milo', age: 5 });
   });
 });
--- week_1_katas/age-dog.js
+++ week_1_katas/age-dog.js
@@ -1,3 +1,16 @@
-const ageDog = (dog, num) => {};
+/*
+This function will take a single dog object with an age property and a number. You should increment the age property by the provided number and return the aged dog. The original dog object must not be mutated!
+
+- solve problem in js
+-refactor using typescript
+*/
+
+const ageDog = (dog, num) => {
+  const copyDog = { ...dog };
+
+  copyDog.age += num;
+
+  return copyDog;
+};
 
 module.exports = ageDog;
