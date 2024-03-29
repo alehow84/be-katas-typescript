41a31ef7a252f3410c05f4b7ff6440603941a9de
Alex Howlett <alex.howlett@live.co.uk>
2024-03-29 16:53:51 +0000

    resolve createRef kata with 2 tests passing

--- week_1_katas/__tests__/create-ref.test.js
+++ week_1_katas/__tests__/create-ref.test.js
@@ -1,7 +1,45 @@
 const createRef = require('../create-ref.js');
 
 describe('createRef', () => {
-  it('', () => {
-    // first test here
+  it('function returns a new object', () => {
+    const startArr = [
+      {
+        name: 'vel',
+        phoneNumber: '01134445566',
+        address: 'Northcoders, Leeds'
+      },
+      {
+        name: 'ant',
+        phoneNumber: '01612223344',
+        address: 'Northcoders, Manchester'
+      },
+      { name: 'mitch', phoneNumber: '07777777777', address: null }
+    ];
+
+    expect(createRef(startArr)).not.toBe(startArr);
+    expect(typeof createRef(startArr)).toBe('object');
+  });
+  it('should return an object each key matches the name value of the person object, and the corresponding value matches the phoneNumber key', () => {
+    const startArr = [
+      {
+        name: 'vel',
+        phoneNumber: '01134445566',
+        address: 'Northcoders, Leeds'
+      },
+      {
+        name: 'ant',
+        phoneNumber: '01612223344',
+        address: 'Northcoders, Manchester'
+      },
+      { name: 'mitch', phoneNumber: '07777777777', address: null }
+    ];
+
+    const expectedObj = {
+      vel: '01134445566',
+      ant: '01612223344',
+      mitch: '07777777777'
+    };
+
+    expect(createRef(startArr)).toEqual(expectedObj);
   });
 });
--- week_1_katas/create-ref.js
+++ week_1_katas/create-ref.js
@@ -1,3 +1,15 @@
-const createRef = () => {};
+/**
+ Create a function called createRef that can convert an array of people into a reference object that can be used to look up a person's phone number from their name.
+ */
+
+const createRef = (people) => {
+  const phoneNumsObj = {};
+
+  people.forEach((person) => {
+    phoneNumsObj[person.name] = person.phoneNumber;
+  });
+
+  return phoneNumsObj;
+};
 
 module.exports = createRef;
