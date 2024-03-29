cd1939ae83f0ee13e196304075d2c62cc5eb2880
Alex Howlett <alex.howlett@live.co.uk>
2024-03-29 16:35:53 +0000

    resolve treatDoggos kata, 5 tests passing

--- week_1_katas/__tests__/treat-doggos.test.js
+++ week_1_katas/__tests__/treat-doggos.test.js
@@ -1,10 +1,44 @@
+const { start } = require('repl');
 const treatDoggos = require('../treat-doggos.js');
 
 describe('treatDoggos', () => {
-  it('should return an array of objects with a different reference to the starting array of objects ', () => {
+  it('should return an array of objects ', () => {
+    const startArrDogs = [{ name: 'Otis', age: 3, hasRabies: true }];
+    expect(treatDoggos(startArrDogs)[0]).toMatchObject({
+      name: 'Otis',
+      age: 3,
+      hasRabies: expect.any(Boolean)
+    });
+  });
+  it('should return an array of dog objects with the boolean value flipped where there is only one dog object', () => {
     const startArrDogs = [{ name: 'Otis', age: 3, hasRabies: true }];
 
-    expect(treatDoggos(startArrDogs)).not.toBe(startArrDogs);
-    expect(typeof treatDoggos(startArrDogs)).toBe('array');
+    expect(treatDoggos(startArrDogs)[0].hasRabies).toBe(false);
+  });
+  it('should return an array of dog objects with each of their boolean values flipped where there are multiple dog objects', () => {
+    const startArrDogs = [
+      { name: 'Otis', age: 3, hasRabies: true },
+      { name: 'Matilda', age: 11, hasRabies: false },
+      { name: 'Milo', age: 3, hasRabies: false }
+    ];
+    expect(treatDoggos(startArrDogs)).toMatchObject([
+      { name: 'Otis', age: 3, hasRabies: false },
+      { name: 'Matilda', age: 11, hasRabies: true },
+      { name: 'Milo', age: 3, hasRabies: true }
+    ]);
+  });
+  it('invoking the function should not mutate the starting array', () => {
+    const startArrDogs = [
+      { name: 'Otis', age: 3, hasRabies: true },
+      { name: 'Matilda', age: 11, hasRabies: false },
+      { name: 'Milo', age: 3, hasRabies: false }
+    ];
+
+    treatDoggos(startArrDogs);
+    expect(startArrDogs).toEqual([
+      { name: 'Otis', age: 3, hasRabies: true },
+      { name: 'Matilda', age: 11, hasRabies: false },
+      { name: 'Milo', age: 3, hasRabies: false }
+    ]);
   });
 });
--- week_1_katas/treat-doggos.js
+++ week_1_katas/treat-doggos.js
@@ -1,7 +1,13 @@
-/*
-This function will take an array of dog objects each with a hasRabies property set to true. It should return a new array of new dog objects. Each dog should have the hasRabies flag flipped to false.
-*/
+const treatDoggos = (dogs) => {
+  let flippedDogsArr = [];
 
-const treatDoggos = (dogs) => {};
+  dogs.forEach((dog) => {
+    let copyDog = { ...dog };
+    copyDog.hasRabies = !copyDog.hasRabies;
+    flippedDogsArr.push(copyDog);
+  });
+
+  return flippedDogsArr;
+};
 
 module.exports = treatDoggos;
