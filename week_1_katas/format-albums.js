1b7c6eb35a7f1b753bca6aaa129609e12e1c2513
Alex Howlett <alex.howlett@live.co.uk>
2024-04-01 18:08:23 +0100

    complete format-albums

--- week_1_katas/format-albums.js
+++ week_1_katas/format-albums.js
@@ -1,3 +1,18 @@
-const formatAlbums = () => {};
+const formatAlbums = (albums, artistIds) => {
+  const updatedAlbums = albums.map((album) => {
+    return { ...album };
+  });
+
+  updatedAlbums.forEach((album) => {
+    let artistID = null;
+    let artistIDArr = Object.keys(artistIds).filter(
+      (key) => album.artist === key
+    );
+    artistID = artistIDArr[0];
+    album.artist = artistIds[artistID];
+  });
+
+  return updatedAlbums;
+};
 
 module.exports = formatAlbums;
