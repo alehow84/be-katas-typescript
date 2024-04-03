8bc59561ebc00a8538f7898cadd0a5483257e3da
Alex Howlett <alex.howlett@live.co.uk>
2024-04-03 12:05:54 +0100

    add functions to shift through an array of index no's to find correct index number for decoded letter in alphaCharArr. Still not working

--- week_2_katas/__tests__/counter-intelligence.test.js
+++ week_2_katas/__tests__/counter-intelligence.test.js
@@ -2,32 +2,30 @@ const { counterIntelligence } = require('../counter-intelligence');
 
 describe('counterIntelligence()', () => {
   it('returns a string', () => {
-    const input = 'Y'
-    const actualOutput = counterIntelligence(input)
+    const input = 'Y';
+    const actualOutput = counterIntelligence(input);
 
     expect(typeof actualOutput).toBe('string');
   });
   it('returns the correct string when the encrypted msg has been shifted by 1 character and is only 1 character long', () => {
-    const input = 'Y'
-    const actualOutput = counterIntelligence(input)
-    const expectedOutput = 'X'
+    const input = 'Y';
+    const actualOutput = counterIntelligence(input);
+    const expectedOutput = 'X';
 
     expect(actualOutput).toBe(expectedOutput);
   });
   it('returns the correct string when the encrypted msg has been shifted by 1 character and has multiple characters', () => {
-    const input = 'BCD Y'
-    const actualOutput = counterIntelligence(input)
-    const expectedOutput = 'ABC X'
+    const input = 'BCD Y';
+    const actualOutput = counterIntelligence(input);
+    const expectedOutput = 'ABC X';
 
-    expect(actualOutput).toBe(expectedOutput)
-
-  })
+    expect(actualOutput).toBe(expectedOutput);
+  });
   it('returns the correct string when the encrypted msg has been shifted by any character and has multiple characters', () => {
-    const input = 'NKRRU YCKKZNKGXZ D'
-    const actualOutput = counterIntelligence(input)
-    const expectedOutput = 'HELLO SWEETHEART X'
+    const input = 'NKRRU YCKKZNKGXZ D';
+    const actualOutput = counterIntelligence(input);
+    const expectedOutput = 'HELLO SWEETHEART X';
 
-    expect(actualOutput).toBe(expectedOutput)
-
-  })
+    expect(actualOutput).toBe(expectedOutput);
+  });
 });
--- week_2_katas/counter-intelligence.js
+++ week_2_katas/counter-intelligence.js
@@ -1,37 +1,63 @@
 const counterIntelligence = (str) => {
-  const lastCharsCode = str.charCodeAt(str.length-1)
-  // console.log(lastCharsCode, 'lastCharsCode on line 3')
-  let charShift = 88 - lastCharsCode
-  // console.log(charShift,'charShift on line 4')
-  const strArr = str.split('')
-  const decodedStrArr = strArr.map((letter)=>{
-    if (letter === ' ') {
-      return letter
-    } else {
-    const letterIndex = strArr.indexOf(letter)
-    let letterCode = strArr.join('').charCodeAt(letterIndex)
-    if(charShift < 0) {
-    letterCode+= charShift 
-    letter = String.fromCharCode(letterCode)
-    return letter
-    } else {
-      // console.log(letter, letterCode, charShift, String.fromCharCode(letterCode - charShift), 'letterCode on line 18')
-      /*
-      something is wrong here, so far for the last test, the X returns as expected, but the difference between the expected char codes and the received char codes is 26.
+  const alphaCharArr = Array.from(Array(26)).map((e, i) => i + 65);
+  const lastCharsCode = str.charCodeAt(str.length - 1);
+  //88 is index 23
+  //below gets the last characters index
+  const lastCharIndex = alphaCharArr.indexOf(lastCharsCode);
+  console.log(lastCharIndex, 'lastCharIndex');
+  //charShift is how much the character has shifted bby
 
-      - possible solution: shifting through the character codes does not work as expected, so use this to efficiently create an alphabet array then shifting through that should work as expected.
-      */
-      letterCode = letterCode + charShift
-      console.log(letterCode, 'letterCode on line 20')
-      letter = String.fromCharCode(letterCode)
-      return letter
+  // console.log(alphaCharArr);
+
+  //find index change between x and the letter
+  let charIndexShift = 23 - lastCharIndex;
+  console.log(charIndexShift, 'charIndexShift');
+
+  //need to find the index of each character in the array
+  //minus the charIndexShift of it to find the index of the actual character code
+  //convert it back into a character and return it
+  const strArr = str.split('');
+  const decodedStrArr = strArr.map((letter) => {
+    if (letter === ' ') {
+      return letter;
+    } else {
+      //finding the letter index in the string ive just turned into an array
+      const letterIndex = strArr.indexOf(letter);
+      //gets the number representing the
+      let letterCode = strArr.join('').charCodeAt(letterIndex);
+      // console.log(letterCode, 'letterCode');
+
+      let currentLetterAlphaCharIndex = alphaCharArr.indexOf(letterCode);
+
+      console.log(currentLetterAlphaCharIndex, 'index');
+
+      currentLetterAlphaCharIndex += charIndexShift;
+      //incorrect - need to find a way to shift the index number along through a range of 0-25
+
+      function range(start, end) {
+        return Array.from({ length: end - start + 1 }, (_, i) => start + i);
+      }
+
+      function rotateArray(arr, shiftAmount) {
+        const amount = shiftAmount % arr.length;
+        return [...arr.slice(amount), ...arr.slice(0, amount)];
+      }
+
+      const startRange = range(0, 25);
+      const rotatedRange = rotateArray(startRange, charIndexShift);
+      const codedIndex = startRange.indexOf(currentLetterAlphaCharIndex);
+      const decodedIndex = rotatedRange[codedIndex];
+      console.log(codedIndex, 'CodedIndex', decodedIndex, 'decodedIndex');
+
+      letter = String.fromCharCode(alphaCharArr[currentLetterAlphaCharIndex]);
+      // letter = String.fromCharCode(alphaCharArr[decodedIndex]);
+      //line 52 fails 3rd test, above on line 53 is almost working - think there is something wrong with how i am trying to calculatr how much to shift by (line 13)
+
+      return letter;
     }
-    }
-    
-  })
-  return decodedStrArr.join('')
-  
-  
+  });
+
+  return decodedStrArr.join('');
 };
 
 module.exports = { counterIntelligence };
